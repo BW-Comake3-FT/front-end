2a7b398b60a5ac6ad669fbff22722c10321fab3c
Javier Umana <javierumanafs@gmail.com>
2020-04-27 14:43:52 -0400

    set up sign up act/red, fixed a few things on signup form

--- src/App.js
+++ src/App.js
@@ -5,13 +5,19 @@ import { Route, Switch } from 'react-router-dom';
 import PrivateRoute from './components/PrivateRoute';
 
 import Login from './components/Login';
+import Signup from './components/Signup';
 
 function App() {
   return (
     <div className="App">
    <h1>App</h1>
    <Switch>
-     <Route exact path='/' component={Login}/>
+     <Route 
+     exact path='/' 
+     component={Login}/>
+     <Route 
+     path='/signup' 
+     component={Signup} />
    </Switch>
     </div>
   );
--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -12,12 +12,33 @@ export const LOGOUT_START = 'LOGOUT_START';
 export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS';
 
 export const login = (credentials, history) => dispatch => {
-  console.log('login action firing');
+  console.log(credentials,'login action firing');
   dispatch({ type: LOGIN_START});
 
   return(
    axios.post('', credentials)
    .then(res => console.log({res}))
    .catch(err => console.log({err}))
+  // console.log({credentials}, 'creds')
   );
 };
+
+export const signup = (newUser, history) => dispatch => {
+  console.log('signup action firing');
+  dispatch({ type: SIGNUP_START });
+  return(
+    axios.post('', newUser)
+    .then(res => console.log(res))
+    // dispatch({ type: SIGNUP_SUCCESS, payload:  })
+    .catch(err => console.log(err))
+    // dispatch({ type: SIGNUP_FAILURE, payload: err });
+  );
+};
+
+export const logout = e => dispatch => {
+  dispatch({ type: LOGOUT_START });
+  e.preventDefault();
+  localStorage.removeItem('token');
+  localStorage.removeItem('userId');
+  dispatch({ type: LOGOUT_SUCCESS })
+}
